5a00aec3bcf482807640344d3e8565a72459d30d
javersek <74268577+javersek@users.noreply.github.com>
2021-12-04 21:01:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 let map;
 
-
-
-
-
 var markers = [];
 
 function initMap(markers) {
@@ -11,7 +7,13 @@ function initMap(markers) {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 46.060976362379066, lng: 14.510848589269866 },
     zoom: 13,
+    mapTypeId: "terrain",
+    
   });
+  
+
+
+  
 
   const styles = {
     hide: [
@@ -155,5 +157,13 @@ var marker;
 
     }
   }
+
+
+  
+
+
   getapi(url);
   getapi2(url2);
+
+  
+  
